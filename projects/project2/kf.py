1268760b25a782f86b71c18f0dd82f144c28fbce
Pedram <psafaei@nevada.unr.edu>
2018-11-21 01:57:50 -0800

    TODO: find better Sigma values

--- projects/project2/kf.py
+++ projects/project2/kf.py
@@ -9,7 +9,7 @@ from scipy import stats
 import scipy.stats
 
 def kf_update(u, sigma, z):
-	deltaT = 1 
+	deltaT = 1
 	x = 6
 	S_z = 4
 	H = np.matrix([1, 0])
@@ -19,7 +19,7 @@ def kf_update(u, sigma, z):
 	F = np.matrix([[1, deltaT], [0, 1]])
 	K = ((F * sigma * F.T) + X) * H.T * np.linalg.inv(H * ((F * sigma * F.T) + X) * H.T + Z)
 	NextSigma = (np.identity(2) - (K * H)) * ((F * sigma * F.T) + X)
-	Next_u = F * u + K * (z - H * F * u) 
+	Next_u = F * u + K * (z - H * F * u)
 	return [Next_u, NextSigma]
 
 def door_update(u, sigma, d, door_dist):
@@ -32,26 +32,7 @@ def door_update(u, sigma, d, door_dist):
 		index = 0
 	S = (door * door_dist[index]) + (1-no_door)*(1-door_dist[index])
 	if d is True:
-		door_dist[index] =  (door_dist[index] * door) / S 
+		door_dist[index] =  (door_dist[index] * door) / S
 	else:
 		door_dist[index]  = 1-(no_door)*(1-door_dist[index])/(1-S)
 	return door_dist
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
