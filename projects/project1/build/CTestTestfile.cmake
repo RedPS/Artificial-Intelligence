5826eaa16d0a3bd3e3eb3ac883ed93e2cfb9488f
Pedram <psafaei@nevada.unr.edu>
2018-11-21 02:01:40 -0800

    cleaning up useless files

--- projects/project1/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ps/Desktop/AI/projects/project1/Proj1/Spam
-# Build directory: /home/ps/Desktop/AI/projects/project1/Proj1/Spam/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
